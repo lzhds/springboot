dd97046de3fa7328d54d171f48431042b163fb1c
lzhds <2430695224@qq.com>
2018-12-20 13:44:24 +0800

    码云Test

--- src/main/java/com/example/springboot/test/gitAdd.java
+++ src/main/java/com/example/springboot/test/gitAdd.java
@@ -3,5 +3,6 @@ package com.example.springboot.test;
 public class gitAdd {
     public static void main(String[] args) {
         System.out.println("添加一点东西上去试一试");
+        System.out,println("mayunTest");
     }
 }
