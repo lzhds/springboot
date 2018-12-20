8ef2519c3683c1c0263fed2468b1fe395884d698
lzhds <2430695224@qq.com>
2018-12-20 13:48:46 +0800

    sad

--- src/main/java/com/example/springboot/test/gitAdd.java
+++ src/main/java/com/example/springboot/test/gitAdd.java
@@ -4,5 +4,6 @@ public class gitAdd {
     public static void main(String[] args) {
         System.out.println("添加一点东西上去试一试");
         System.out.println("mayunTest");
+        String s="123456789";
     }
 }
